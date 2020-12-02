c5896633d3cdb584b37a2c727c82d33f0d92bc50
Atasan <atageldi194229@gmail.com>
2020-12-02 13:55:50 +0500

    connect controllers

--- controllers/v1/index.js
+++ controllers/v1/index.js
@@ -1,11 +1,17 @@
 "use strict";
 
 const AuthController = require("./auth.controller");
+const TestController = require("./test.controller");
 const QuestionController = require("./question.controller");
 const PriceController = require("./price.controller");
+const GroupController = require("./group.controller");
+const UserController = require("./user.controller");
 
 module.exports = {
   AuthController,
+  TestController,
   QuestionController,
   PriceController,
+  GroupController,
+  UserController,
 };
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -4,10 +4,12 @@ const express = require("express");
 const ApiV1Router = express.Router();
 
 const AuthRouter = require("./auth.router");
+const TestRouter = require("./test.router");
 const QuestionRouter = require("./question.router");
 const PriceRouter = require("./price.router");
 
 ApiV1Router.use("/", AuthRouter);
+ApiV1Router.use("/tests", TestRouter);
 ApiV1Router.use("/questions", QuestionRouter);
 ApiV1Router.use("/price", PriceRouter);
 
