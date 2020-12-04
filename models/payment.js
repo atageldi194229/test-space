0f82ecb9eb99e4b4be79220393f80a8bc195c965
Atasan <atageldi194229@gmail.com>
2020-12-04 18:35:35 +0500

    add new method for Payment model (balance)

--- models/payment.js
+++ models/payment.js
@@ -79,6 +79,16 @@ module.exports = (sequelize, DataTypes) => {
   };
   // Custom methods
   Payment.prototype.balance = function (userId) {
+    let query = `SELECT
+    CASE WHEN SUM(is_tsc_unlimited) >= 1 THEN 'UNLIMITED' ELSE SUM(tsc - tsc_used) END TSC ,
+    CASE WHEN SUM(is_tcc_unlimited) >= 1 THEN 'UNLIMITED' ELSE SUM(tcc - tcc_used) END TCC
+    FROM \`payments\` WHERE status = 1 and user_id = ${userId} and DATEDIFF(NOW(), allowed_at) <= 30`;
+    
+    let res = await sequelize.query(query);
+    let data = res[0][0];
+    console.log(data);
+
+    return data;
     // SELECT
     //   CASE WHEN SUM(is_tsc_unlimited) >= 1 THEN 'UNLIMITED' ELSE SUM(tsc - tsc_used) END TSC ,
     //   CASE WHEN SUM(is_tcc_unlimited) >= 1 THEN 'UNLIMITED' ELSE SUM(tcc - tcc_used) END TCC
