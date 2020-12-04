a4ed12a97a110a6b5c4da47e8e91288efc550acc
Atasan <atageldi194229@gmail.com>
2020-12-04 16:18:51 +0500

    create init rows for price [seeders/addSomeRows.js]

--- seeders/addSomeRows.js
+++ seeders/addSomeRows.js
@@ -1,15 +1,65 @@
-const { User } = require("../models");
+const { User, Price } = require("../models");
 
 (async () => {
-  await User.create({
-    username: "atasan",
-    password: User.hashPassword("atasan"),
-    birthDate: "01-01-2019",
-    firstName: "qwerty",
-    lastName: "qwerty",
-    phoneNumber: "+99368597458",
-    email: "qwerty@gmail.com",
-    active: true,
+  // await User.create({
+  //   username: "atasan",
+  //   password: User.hashPassword("atasan"),
+  //   birthDate: "01-01-2019",
+  //   firstName: "qwerty",
+  //   lastName: "qwerty",
+  //   phoneNumber: "+99368597458",
+  //   email: "qwerty@gmail.com",
+  //   active: true,
+  // });
+
+  await Price.create({
+    type: 0,
+    data: JSON.stringify([
+      {
+        ranges: {
+          start: 0,
+          end: 50,
+        },
+        price: 2,
+      },
+      {
+        ranges: {
+          start: 50,
+          end: 100,
+        },
+        price: 1,
+      },
+    ]),
+  });
+
+  await Price.create({
+    type: 1,
+    data: JSON.stringify([
+      {
+        ranges: {
+          start: 0,
+          end: 50,
+        },
+        price: 2,
+      },
+      {
+        ranges: {
+          start: 50,
+          end: 100,
+        },
+        price: 1,
+      },
+    ]),
+  });
+
+  await Price.create({
+    type: 2,
+    data: 200,
+  });
+
+  await Price.create({
+    type: 3,
+    data: 200,
   });
 
   console.log("The End.");
