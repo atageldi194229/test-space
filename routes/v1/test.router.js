2942e301e1479698c4bc9fa399aa411666c55acb
Atasan <atageldi194229@gmail.com>
2020-12-05 17:53:13 +0500

    new api get one test with it's questions

--- routes/v1/test.router.js
+++ routes/v1/test.router.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, getAll } = require("../../controllers").V1.TestController;
+const {
+  create,
+  getAll,
+  getOne,
+} = require("../../controllers").V1.TestController;
 
 const { getUser } = require("../../middleware").JwtMiddleware;
 
 router.get("/", getAll);
 router.post("/", getUser, create);
+router.get("/:id", getUser, getOne);
 
 module.exports = router;
