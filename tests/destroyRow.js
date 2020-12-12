33ade81f9551ee90abd7c796a9fad86d6a068b80
Atasan <atageldi194229@gmail.com>
2020-12-12 16:15:15 +0500

    Update destroyRow.js
    experiment don't mind

--- tests/destroyRow.js
+++ tests/destroyRow.js
@@ -1,1 +1,25 @@
+const {
+  groupsmigration,
+} = require("googleapis/build/src/apis/groupsmigration");
 const { Group, GroupUser, User } = require("../models");
+
+(async () => {
+  let group = await Group.create({
+    name: "group",
+    description: "group",
+    userId: 1,
+  });
+
+  await GroupUser.create({
+    groupId: group.id,
+    userId: 1,
+  });
+
+  // destroy group and see if GroupUser Row is destroyed
+  await Group.destroy({ where: { id: group.id } });
+
+  // experiment is successfull, created GroupUser row was deleted
+
+  // exit process
+  process.exit();
+})();
