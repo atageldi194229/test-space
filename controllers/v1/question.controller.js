982d4032726bfc2d444ed3c062a5ec564edc1116
Atasan <atageldi194229@gmail.com>
2020-12-12 22:34:14 +0500

    Update question.controller.js
    edit api controller created

--- controllers/v1/question.controller.js
+++ controllers/v1/question.controller.js
@@ -9,13 +9,14 @@ const {
 const asyncHandler = require("../../middleware/async");
 const ErrorResponse = require("../../utils/errorResponse");
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 const obj = {};
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function saveFilesOfQuestion(type, data, files) {
+function saveFilesOfQuestion(type, data, files, edit = false) {
   const saveFile = (file) => {
     let fileName = uuidv4() + file.name;
     file.mv("public/uploads/question/" + fileName);
@@ -24,16 +25,20 @@ function saveFilesOfQuestion(type, data, files) {
 
   if (type < 0 || type > 5) return -1;
 
-  if (data.question.audio)
+  if ((edit && data.question.newAudio) || data.question.audio)
     data.question.audio = saveFile(files[data.question.audio]);
-  if (data.question.image)
+  if ((edit && data.question.newImage) || data.question.image)
     data.question.image = saveFile(files[data.question.image]);
+
+  delete data.question.newAudio;
+  delete data.question.newImage;
 
   if (type === 0 || type === 1) {
     // if singleChoice or multiChoice
     for (let i = 0; i < data.answers.length; i++) {
-      if (data.answers[i].type === "image")
+      if ((edit && data.answers[i].new) || data.answers[i].type === "image")
         data.answers[i].value = saveFile(files[data.answers[i].value]);
+      delete data.answers[i].new;
     }
   }
 
@@ -61,9 +66,24 @@ function randomizeMatchingQuestion(data) {
   return data;
 }
 
-obj.create = async (req, res, next) => {
-  console.log(JSON.stringify(req.body, null, 2));
+function deleteFiles(files) {
+  try {
+    if (!files) return;
+    for (let i = 0; i < files.length; i++) {
+      fs.unlinkSync("public" + files[i]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+/**
+ * Create question
+ * action - /v1/questions
+ * method - post
+ * token
+ */
+obj.create = async (req, res, next) => {
   // client data
   let { data, type, isRandom, testId } = req.body;
 
@@ -72,6 +92,7 @@ obj.create = async (req, res, next) => {
 
   // error test
   if (data === -1) return next(new ErrorResponse("Invalid question type"));
+  if (!testId) return next(new ErrorResponse("Invalid testId"));
 
   // if question type is 'matching' let's randomize it's answers
   if (type === 4) randomizeMatchingQuestion(data);
@@ -83,6 +104,42 @@ obj.create = async (req, res, next) => {
     isRandom,
     testId,
   });
+
+  // client response
+  res.status(200).json({
+    success: true,
+  });
+};
+
+/**
+ * Update question, type can't be changed
+ * action - /v1/questions/:id
+ * method - put
+ * token
+ */
+obj.update = async (req, res, next) => {
+  // client data
+  let id = req.params.id;
+  let { data, isRandom, testId, deletedFiles } = req.body;
+  let newData = { isRandom, testId };
+
+  // request db
+  let question = await Question.findOne({ where: { id } });
+
+  // save files of data if exists and delete trashed files
+  if (data) {
+    data = saveFilesOfQuestion(type, JSON.parse(data), req.files, true);
+    deleteFiles(JSON.parse(deleteFiles));
+
+    // if question type is 'matching' let's randomize it's answers
+    if (qusetion.type === 4) randomizeMatchingQuestion(data);
+
+    // add to the new data
+    newData.data = JSON.stringify(data);
+  }
+
+  // update request db
+  await qusetion.update(newData, { where: { id } });
 
   // client response
   res.status(200).json({
