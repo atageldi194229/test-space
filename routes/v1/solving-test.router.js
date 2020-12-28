34bdc85ce686f87944079f4d5ebb4d4d63a47f75
Atasan <atageldi194229@gmail.com>
2020-12-29 04:48:24 +0500

    Update solving-test.router.js
    solve test controller connected to the router

--- routes/v1/solving-test.router.js
+++ routes/v1/solving-test.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAll } = require("../../controllers").V1.TestController;
+const { getAll, solveTest } = require("../../controllers").V1.TestController;
 
 const { verify } = require("../../middleware").JwtMiddleware;
 
 router.get("/", verify, getAll);
+router.get("/v1/solving-tests/:id/solve", verify, solveTest);
 
 module.exports = router;
