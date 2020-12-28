a2ab21ce44c9c6ecec364a03d0c3616080be216b
Atasan <atageldi194229@gmail.com>
2020-12-29 03:26:20 +0500

    Update user.router.js
    get and update my account data controllers are connected to the routes

--- routes/v1/user.router.js
+++ routes/v1/user.router.js
@@ -5,11 +5,15 @@ const router = express.Router();
 const {
   findUsers,
   getOneByName,
+  getMyAccount,
+  updateMyAccount,
 } = require("../../controllers").V1.UserController;
 
 // const { getUser } = require("../../middleware").JwtMiddleware;
 
 router.post("/find", findUsers);
+router.get("/my/account", getMyAccount);
+router.post("/my/account", updateMyAccount);
 router.get("/:username", getOneByName);
 
 module.exports = router;
