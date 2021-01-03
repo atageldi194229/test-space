3ee53bd6f331036c41481130e58c293449fa9079
Atasan <atageldi194229@gmail.com>
2021-01-03 13:31:55 +0500

    Update mailer.service.js
    much secure

--- services/mailer.service.js
+++ services/mailer.service.js
@@ -1,63 +1,64 @@
 "use strict";
 
 const nodemailer = require("nodemailer");
-const mailerConfig = require("../config/mailer");
+const { google } = require("googleapis");
+
+const mailer = require("../config/mailer.json");
+
+const oAuth2Client = new google.auth.OAuth2(
+  mailer.OAuth2.clientId,
+  mailer.OAuth2.clientSecret,
+  mailer.OAuth2.redirectUri,
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 class Mailer {
   constructor() {
+    const accessToken = await oAuth2Client.getAccessToken();
     // create reusable transporter object using the default SMTP transport
     this.transporter = nodemailer.createTransport({
-      host: mailerConfig.host,
-      port: mailerConfig.port,
-      secure: mailerConfig.secure, // true for 465, false for other ports
+      service: "gmail",
+      host: mailer.host,
+      port: mailer.port,
+      secure: mailer.secure, // true for 465, false for other ports
       auth: {
-        user: mailerConfig.user, // testAccount.user, // generated ethereal user
-        pass: mailerConfig.pass, // testAccount.pass, // generated ethereal password
+        type: "OAuth2",
+        user: mailer.user,
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken, 
+        // user: mailerConfig.user, // testAccount.user, // generated ethereal user
+        // pass: mailerConfig.pass, // testAccount.pass, // generated ethereal password
       },
     });
   }
 
   async sendMail({ to, subject, text }) {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    // let testAccount = await nodemailer.createTestAccount();
-
-    // send mail with defined transport object
     let info = await this.transporter.sendMail({
-      from: `"Test Space 👻" <${mailerConfig.user}>`, // sender address
-      to, // "bar@example.com, baz@example.com", // list of receivers
-      subject, // Subject line
-      text, // plain text body
-      // html: "<b>Hello world?</b>", // html body
+      from: `"Test Space 👻" <${mailer.user}>`,
+      to,
+      subject,
+      text,
     });
 
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   }
 
   async sendVerificationCode(obj) {
-    // send mail with defined transport object
     let info = await this.transporter.sendMail({
-      from: `"Test Space 👻✉📝" <${mailerConfig.user}>`, // sender address
-      to: `${obj.to}`, // "bar@example.com, baz@example.com", // list of receivers
-      subject: "Verify your account", // Subject line
-      // text: "Authentication", // plain text body
+      from: `"Test Space 👻✉📝" <${mailer.user}>`,
+      to: `${obj.to}`,
+      subject: "Verify your account",
       html: `
         <b>Verification link</b> 
         <a href="http://192.168.5.99:3007/v1/verify-code/${obj.verifyCode}">${obj.verifyCode}</a>
-      `, // html body
+      `,
     });
 
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   }
 
   async sendTestInvitation({ to, link, userId }) {
@@ -74,17 +75,3 @@ class Mailer {
 }
 
 module.exports = new Mailer();
-
-/*
-
-let message = {
-    ...
-    html: 'Embedded image: <img src="cid:unique@nodemailer.com"/>',
-    attachments: [{
-        filename: 'image.png',
-        path: '/path/to/file',
-        cid: 'unique@nodemailer.com' //same cid value as in the html img src
-    }]
-}
-
-*/
