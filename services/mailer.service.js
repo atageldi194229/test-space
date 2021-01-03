ff25b61fc67587578f56f38529052f85e80007e8
Atasan <atageldi194229@gmail.com>
2021-01-03 22:00:08 +0500

    Update mailer.service.js
    big problems solved

--- services/mailer.service.js
+++ services/mailer.service.js
@@ -10,50 +10,56 @@ const oAuth2Client = new google.auth.OAuth2(
   mailer.OAuth2.clientSecret,
   mailer.OAuth2.redirectUri
 );
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+oAuth2Client.setCredentials({ refresh_token: mailer.OAuth2.refreshToken });
 
 class Mailer {
-  async constructor() {
-    const accessToken = await oAuth2Client.getAccessToken();
-    // create reusable transporter object using the default SMTP transport
-    this.transporter = nodemailer.createTransport({
+  constructor() {}
+
+  async init() {
+    const transporterOptions = {
       service: "gmail",
-      host: mailer.host,
-      port: mailer.port,
-      secure: mailer.secure, // true for 465, false for other ports
+      // host: mailer.host,
+      // port: mailer.port,
+      // secure: mailer.secure,
       auth: {
         type: "OAuth2",
         user: mailer.user,
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
-        accessToken,
-        // user: mailerConfig.user, // testAccount.user, // generated ethereal user
-        // pass: mailerConfig.pass, // testAccount.pass, // generated ethereal password
+        clientId: mailer.clientId,
+        clientSecret: mailer.clientSecret,
+        refreshToken: mailer.refreshToken,
+        accessToken: (await oAuth2Client.getAccessToken()).token,
       },
-    });
+    };
+
+    this.transporter = nodemailer.createTransport(transporterOptions);
   }
 
   async sendMail({ to, subject, text }) {
-    let info = await this.transporter.sendMail({
+    await this.init();
+
+    let payload = {
       from: `"Test Space 👻" <${mailer.user}>`,
       to,
       subject,
       text,
-    });
+    };
+
+    let info = await this.transporter.sendMail(payload);
 
     console.log("Message sent: %s", info.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   }
 
   async sendVerificationCode(obj) {
+    await this.init();
+
     let info = await this.transporter.sendMail({
       from: `"Test Space 👻✉📝" <${mailer.user}>`,
       to: `${obj.to}`,
       subject: "Verify your account",
       html: `
         <b>Verification link</b> 
-        <a href="http://192.168.5.99:3007/v1/verify-code/${obj.verifyCode}">${obj.verifyCode}</a>
+        <a href="http://testspace.com.tm/api/v1/verify-code/${obj.verifyCode}">${obj.verifyCode}</a>
       `,
     });
 
@@ -62,9 +68,10 @@ class Mailer {
   }
 
   async sendTestInvitation({ to, link, userId }) {
-    let str = "";
-    if (to.length > 0) str += to[0];
-    for (let i = 1; i < to.length; i++) str += `,${to[i]}`;
+    let str = to.join();
+
+    // if (to.length > 0) str += to[0];
+    // for (let i = 1; i < to.length; i++) str += `,${to[i]}`;
 
     await this.sendMail({
       to: str,
