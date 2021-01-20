b11b082e42667a610adf447254a3a52ab6137da4
Atasan <atageldi194229@gmail.com>
2021-01-20 17:32:42 +0500

    Update user-result.router.js
    added new route

--- routes/v1/user-result.router.js
+++ routes/v1/user-result.router.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
   solveQuestion,
   removeSolvedQuestion,
+  getSolvedQuestions,
 } = require("../../controllers").V1.UserResultController;
 
 const { verify } = require("../../middleware").JwtMiddleware;
 
 router.post("/:id/questions/:cid", verify, solveQuestion);
 router.delete("/:id/questions/:cid", verify, removeSolvedQuestion);
+router.get("/:id/questions", verify, getSolvedQuestions);
 
 module.exports = router;
